ab86ace17ad5dbbf458c073e781d3787a1b4172c
Shawn McKay <snmckay@users.noreply.github.com>
2021-11-16 12:33:55 -0600

    Making a simple graph

--- strong_NMOS_final.py
+++ strong_NMOS_final.py
@@ -11,6 +11,21 @@ import serial
 import time
 #from data_logger import DataLogging
 #import excelWrite as xls
+import matplotlib.pyplot as plt
+
+X = [590,540,740,130,810,300,320,230,470,620,770,250]
+Y = [32,36,39,52,61,72,77,75,68,57,48,48]
+
+plt.scatter(X,Y)
+
+plt.xlim(0,1000)
+plt.ylim(0,100)
+
+plt.title('Relationship Between Temperature and Iced Coffee Sales')
+plt.xlabel('Cups of Iced Coffee Sold')
+plt.ylabel('Temperature in Fahrenheit')
+
+plt.show()
 
 
 SOURCE_SETUP = """
